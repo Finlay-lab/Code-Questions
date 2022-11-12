fab03effe538fcac1296d352c75a8b041f28bec5
Finlay-lab <85559037+Finlay-lab@users.noreply.github.com>
2022-11-12 16:47:16 +0300

    Add files via upload

---
+++ Area-Perimeter.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //variable declaration
+    float area,perimeter;
+    int base,height,hypotenuse
+
+    //variable initialization
+    base=7;
+    height=10;
+    hypotenuse=12;
+
+    area= 0.5*base*height;
+    perimeter= base+height+hypotenuse;
+
+//output
+cout<<"area:"<<area<<endl;
+cout<<"perimeter:"<<perimeter<<endl;
+}
